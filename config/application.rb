be960abc101ca98df25757373d3a337511c0267b
HaroonMasihDevDen <Haroon.Masih@devden.io>
2024-07-30 17:17:48 +0500

    add active admin for rails app

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,13 @@
 require_relative "boot"
 
 require "rails/all"
+require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module EcommerceBackend
+module PracticeProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
@@ -28,5 +29,9 @@ module EcommerceBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
