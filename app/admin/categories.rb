803054e5c6b361efd948b75d61d1c77aa208e01d
HaroonMasihDevDen <Haroon.Masih@devden.io>
2024-08-05 13:05:53 +0500

    add category into active_admin resources

---
+++ app/admin/categories.rb
@@ -0,0 +1,35 @@
+ActiveAdmin.register Category do
+
+  filter :name
+  filter :created_at
+  filter :updated_at
+  filter :parent_category_id, as: :select, collection: -> { Category.pluck(:name, :id).map { |name, id| [name, id] } }
+
+  permit_params :name, :description, :parent_category_id
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :description
+    column :parent_category_id
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :description
+      f.input :parent_category_id, as: :select, collection: Category.all.collect { |c| [c.name, c.id] }
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :description
+      row :parent_category_id
+    end
+  end
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,12 @@ class Category < ApplicationRecord
   has_many :products, through: :category_products
   belongs_to :parent_category, class_name: 'Category', optional: true
   has_many :subcategories, class_name: 'Category', foreign_key: 'parent_category_id', dependent: :destroy
+
+  def self.ransackable_attributes(auth_object = nil)
+    ['name','description', 'parent_category_id' , 'created_at','updated_at', 'deleted_at']
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["category_products", "parent_category", "products", "subcategories"]
+  end
 end
