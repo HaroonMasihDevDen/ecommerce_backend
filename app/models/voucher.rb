f2a7a97091f245b26af6c576e545aeabe522d8e2
HaroonMasihDevDen <haroon.masih@devden.io>
2024-08-14 13:18:31 +0500

    add validations to voucher model

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -1,6 +1,16 @@
 class Voucher < ApplicationRecord
+  include ::Ransackable
+
+  validates :token, presence: true, uniqueness: true
+  validates :applied_type, :discount_type, presence: true
+  validates :discount_value , presence: true
+  validates :cap_amount, presence: true , numericality: true
+
   has_many :order_vouchers, dependent: :destroy
   has_many :orders, through: :order_vouchers
   has_many :order_product_vouchers, dependent: :destroy
   has_many :order_products, through: :order_product_vouchers
+
+
+
 end
